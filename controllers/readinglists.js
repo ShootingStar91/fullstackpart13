7dc6c2049effd5ef2a9f1356885faa16e7083408
arttu <ttu91@hotmail.com>
2022-12-13 17:40:01 +0200

    13.22 done

--- controllers/readinglists.js
+++ controllers/readinglists.js
@@ -1,5 +1,6 @@
-const { ReadingList } = require('../models')
+const { ReadingList, Blog } = require('../models')
 const router = require('express').Router()
+const { userExtractor } = require("../util/middleware");
 
 router.post('/api/readinglists', async (req, res) => {
     const readinglistAddition = await ReadingList.create({
@@ -7,6 +8,16 @@ router.post('/api/readinglists', async (req, res) => {
         userId: req.body.user_id
     })
     res.json(readinglistAddition)
+})
+
+router.put('/api/readinglists/:id', userExtractor, async (req, res) => {
+  const readingListEntry = await ReadingList.findByPk(req.params.id);
+  if (readingListEntry.userId !== req.user.id) {
+    throw new Error("You can only change your own readinglists!");
+  }
+  readingListEntry.read = req.body.read;
+  await readingListEntry.save();
+  res.json(readingListEntry);
 })
 
 
